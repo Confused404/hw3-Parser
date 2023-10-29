eb4f9e638bf1bdc7522025e792227677fb8c2f9a
Eduardo Vila <eduardovila57@gmail.com>
2023-10-29 17:19:13 -0400

    Lot of copying

--- symtab.h
+++ symtab.h
@@ -1,35 +1,34 @@
-#ifndef _SYMTAB_H
-#define _SYMTAB_H
+#ifndef _SCOPE_SYMTAB_H
+#define _SCOPE_SYMTAB_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "parser.h"
-#include "lexer.h"
+#include <stdbool.h>
+#include "token.h"
 #include "ast.h"
-#include "scope_check.h"
-#include "utilities.h"
-#include "unparser.h"
 #include "id_attrs.h"
-#include "id_use.h"
 
+// Maximum number of declarations that can be stored in a scope
 #define MAX_SCOPE_SIZE 4096
 
-extern void initialize();
+// initialize the symbol table for the current scope
+extern void scope_initialize();
 
-extern int size();
+// Return the current scope's next offset to use for allocation,
+// which is the size of the current scope (number of declared ids).
+extern unsigned int scope_size();
 
-extern bool full();
+// Is the current scope full?
+extern bool scope_full();
 
-extern bool declared(char * name);
+// Is the given name associated with some attributes in the current scope?
+extern bool scope_defined(const char *name);
 
-extern id_use *lookup(char * name);
+// Requires: !scope_defined(name) && attrs != NULL;
+// Modify the current scope symbol table to
+// add an association from the given name to the given id_attrs attrs.
+extern void scope_insert(const char *name, id_attrs *attrs);
 
-extern bool declared_in_current_scope(char * name);
-
-extern void insert( char * name, id_attrs *attrs);
-
-extern void enter_scope();
-
-extern void leave_scope();
+// Return (a pointer to) the attributes of the given name in the current scope
+// or NULL if there is no association for name.
+extern id_attrs *scope_lookup(const char *name);
 
 #endif
