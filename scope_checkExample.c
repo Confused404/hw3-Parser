063a4a1d1a5f4b1803054c7ce5dff80ee1776bb5
Alex <alexander.leventis@ucf.edu>
2023-11-01 13:35:21 -0400

    split up the example code and real code

---
+++ scope_checkExample.c
@@ -0,0 +1,184 @@
+// EXAMPLE CODE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "scope_check.h"
+#include "id_attrs.h"
+#include "file_location.h"
+#include "ast.h"
+#include "utilities.h"
+#include "symtab.h"
+#include "scope_check.h"
+
+// Build the symbol table for the given program AST
+// and Check the given program AST for duplicate declarations
+// or uses of identifiers that were not declared
+void scope_check_program(AST *prog)
+{
+    symtab_enter_scope(); // // //we are missing a definition for this function
+    
+    scope_check_varDecls(prog->data.program.vds);
+    scope_check_stmt(prog->data.program.stmt);
+
+    symtab_leave_scope(); // // //we are missing a definition for this function
+}
+
+// build the symbol table and check the declarations in vds
+void scope_check_varDecls(AST_list vds)
+{
+    while (!ast_list_is_empty(vds)) {
+	scope_check_varDecl(ast_list_first(vds)); // // // ast_list_first is not defined
+	vds = ast_list_rest(vds);                 // // // ast_list_rest is not defined
+    }
+}
+
+// check the var declaration vd
+// and add it to the current scope's symbol table
+// or produce an error if the name has already been declared
+void scope_check_varDecl(AST *vd)
+{
+    int ofst = symtab_next_loc_offset(); // // // symtab_next_loc_offset is not defined
+    id_attrs *attrs = id_attrs_loc_create(vd->file_loc, vd->data.var_decl.vt, ofst); // // // id_attrs_loc_create is not defined
+    const char *name = vd->data.var_decl.name;
+    symtab_insert(name, attrs);
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_stmt(AST *stmt)
+{
+    switch (stmt->type_tag) {
+    case assign_ast:
+	scope_check_assignStmt(stmt);
+	break;
+    case begin_ast:
+	scope_check_beginStmt(stmt);
+	break;
+    case if_ast:
+	scope_check_ifStmt(stmt);
+	break;
+    case read_ast:
+	scope_check_readStmt(stmt);
+	break;
+    case write_ast:
+	scope_check_writeStmt(stmt);
+	break;
+    default:
+	bail_with_error("Call to scope_check_stmt with an AST that is not a statement!");
+	break;
+    }
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_assignStmt(AST *stmt)
+{
+    const char *name = stmt->data.assign_stmt.ident->data.ident.name;
+    id_use *idu = scope_check_ident_is_data(stmt->file_loc, name);
+    stmt->data.assign_stmt.ident->data.ident.idu = idu;
+    scope_check_expr(stmt->data.assign_stmt.exp);
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_beginStmt(AST *stmt)
+{
+    symtab_enter_scope(); // // // symtab_enter_scope is not defined
+    scope_check_varDecls(stmt->data.begin_stmt.vds);
+    AST_list stmts = stmt->data.begin_stmt.stmts;
+    while (!ast_list_is_empty(stmts)) {
+	scope_check_stmt(ast_list_first(stmts)); // // // ast_list_first is not defined
+	stmts = ast_list_rest(stmts);            // // // ast_list_rest is not defined
+    }
+    symtab_leave_scope();                    // // // symtab_leave_scope is not defined
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_ifStmt(AST *stmt)
+{
+    scope_check_expr(stmt->data.if_stmt.exp);
+    scope_check_stmt(stmt->data.if_stmt.stmt);
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_readStmt(AST *stmt)
+{
+    stmt->data.read_stmt.ident->data.ident.idu
+	= scope_check_ident_is_data(stmt->file_loc,
+				    stmt->data.read_stmt.ident->data.ident.name);
+}
+
+// check the statement to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_writeStmt(AST *stmt)
+{
+    scope_check_expr(stmt->data.write_stmt.exp);
+}
+
+// check the expresion to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_expr(AST *exp)
+{
+    switch (exp->type_tag) {
+    case ident_ast:
+	{   // must update the id_use in the AST
+	    id_use *idu
+		= scope_check_ident_is_data(exp->file_loc,
+					    exp->data.ident.name);
+	    exp->data.ident.idu = idu;
+	}
+	break;
+    case bin_expr_ast:
+	scope_check_bin_expr(exp);
+	break;
+    case number_ast:
+	// no identifiers are possible in this case, so just return
+	break;
+    case not_expr_ast:
+	scope_check_expr(exp->data.not_expr.exp);
+	break;
+    default:
+	bail_with_error("Unexpected type_tag (%d) in scope_check_expr (for line %d, column %d)!",
+			exp->type_tag, exp->file_loc.line, exp->file_loc.column);
+	break;
+    }
+}
+
+// check the expression (exp) to make sure that
+// all idenfifiers referenced in it have been declared
+// (if not, then produce an error)
+void scope_check_bin_expr(AST *exp)
+{
+    scope_check_expr(exp->data.bin_expr.leftexp);
+    scope_check_expr(exp->data.bin_expr.rightexp);
+}
+
+// check that the given name has been declared,
+// if so, then return an id_use (containing the attributes) for that name,
+// otherwise, produce an error using the file_location (floc) given.
+static id_use *scope_check_ident_declared(file_location floc, const char *name)
+{
+    id_use *ret = symtab_lookup(name);
+    if (ret == NULL) {
+	general_error(floc, "identifier \"%s\" is not declared!", name); // // // general_error is not defined
+    }
+    return ret;
+}
+
+// check that the given constant or variable name has been declared,
+// if so, then return an id_use (containing the attributes) for that name,
+// otherwise, produce an error using the file_location (floc) given.
+id_use *scope_check_ident_is_data(file_location floc, const char *name)
+{
+    return scope_check_ident_declared(floc, name);
+}
--- symtab.c
+++ symtab.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 #include "symtab.h"
-#include "utilities.h"
 
 typedef struct
 {
@@ -11,115 +6,76 @@ typedef struct
     id_attrs *attrs;
 } symtab_assoc_t;
 
-// Invariant: 0 <= size < MAX_SCOPE_SIZE;
 typedef struct scope_symtab_s
 {
     unsigned int size;
     symtab_assoc_t *entries[MAX_SCOPE_SIZE];
 } scope_symtab_t;
 
-// The current scope (i.e., the symbol table)
-// Idea: size is the index into entries
-// of the most recent symtab_assoc added to the list of entries.
-// So the next entry goes into the index size+1
 static scope_symtab_t *symtab = NULL;
 
-// Allocate a fresh scope symbol table and return (a pointer to) it.
-// Issues an error message (on stderr) if there is no space
-// and exits with a failure error code in that case.
-static scope_symtab_t *symtab_create()
-{
-    scope_symtab_t *new_scope = (scope_symtab_t *)malloc(sizeof(scope_symtab_t));
-    if (new_scope == NULL)
-    {
-        bail_with_error("No space for new scope_symtab_t!");
-    }
-    new_scope->size = 0;
-    for (int j; j < MAX_SCOPE_SIZE; j++)
-    {
-        new_scope->entries[j] = NULL;
-    }
-    return new_scope;
-}
-
-// initialize the symbol table for the current scope
 void symtab_initialize()
 {
-    // create the scope and assign it to the global symtab
-    symtab = symtab_create();
+    symtab = (scope_symtab_t *)malloc(sizeof(scope_symtab_t));
+    if(symtab == NULL)
+    {
+        bail_with_error("No space for symbol table!");
+    }
+    symtab->size = 0;
+    for(int i = 0; i < MAX_SCOPE_SIZE; i++)
+    {
+        symtab->entries[i] = NULL;
+    }
 }
 
-// Return the current scope's next offset to use for allocation,
-// which is the size of the current scope (number of declared ids).
-unsigned int symtab_size()
+int size()
 {
     return symtab->size;
 }
 
-// Is the current scope full?
-bool symtab_full()
+bool full()
 {
-    return symtab_size() >= MAX_SCOPE_SIZE;
+    return size() >= MAX_SCOPE_SIZE;
 }
 
-// Requires: assoc != NULL && !symtab_full() && !symtab_declared(assoc->id);
-// Add an association from the given name to the given id attributes
-// in the current scope.
-static void symtab_add(symtab_assoc_t *assoc)
+bool declared(const char * name)
 {
-    // assert(assoc != NULL);
-    // assert(!symtab_full());
-    // assert(!symtab_declared(assoc->id));
-    symtab->entries[symtab->size] = assoc;
-    symtab->size++;
-}
-
-// Requires: !symtab_declared(name) && attrs != NULL;
-// Modify the current scope symbol table to
-// add an association from the given name to the given id_attrs attrs.
-void symtab_insert(const char *name, id_attrs *attrs)
-{
-    // assert(!symtab_declared(name));
-    // assert(attrs != NULL);
-    symtab_assoc_t *new_assoc = malloc(sizeof(symtab_assoc_t));
-    if (new_assoc == NULL)
+    if(lookup(name) == NULL)
     {
-        bail_with_error("No space for association!");
+        return false;
     }
-    new_assoc->id = name;
-    new_assoc->attrs = attrs;
-    symtab_add(new_assoc);
+    return true;
 }
 
-// Requires: name != NULL;
-// Is the given name associated with some attributes in the current scope?
-bool symtba_declared(const char *name)
+id_attrs lookup(const char * name)
 {
-    // assert(symtab != NULL);
-    // assert(name != NULL);
-    return symtab_lookup(name) != NULL;
-}
-
-// Requires: name != NULL and symtab_initialize() has been called previously.
-// Return (a pointer to) the attributes of the given name in the current scope
-// or NULL if there is no association for name.
-id_attrs *symtab_lookup(const char *name)
-{
-    int i;
-    // assert(name != NULL);
-    // assert(symtab != NULL);
-    for (i = 0; i < symtab->size; i++)
+    for(int i = 0; i < size(); i++)
     {
-        // assert(symtab != NULL);
-        // assert(symtab->entries != NULL);
-        // assert(0 <= i && i < symtab->size);
-        // assert(symtab->entries[i] != NULL);
-        // assert(symtab->entries[i]->id != NULL);
-        if (strcmp(symtab->entries[i]->id, name) == 0)
+        if(strcmp(symtab->entries[i]->id, name) == 0)
         {
-            return symtab->entries[i]->attrs;
+            return &symtab->entries[i]->attrs;
         }
     }
-    // assert(i == symtab->size);
     return NULL;
 }
+
+bool declared_in_current_scope(const char * name)
+{
+
+}
+
+void insert(const char * name, id_attrs * attrs)
+{
+
+}
+
+void enter_scope()
+{
+
+}
+
+void leave_scope()
+{
+
+}
+
